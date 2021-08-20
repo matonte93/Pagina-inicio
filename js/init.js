9328754fce200984a1af6b4f8bfff91f0f3d321b
matonte93 <nicocaraballo1212@gmail.com>
2021-08-20 08:18:18 -0300

    2

--- js/init.js
+++ js/init.js
@@ -44,7 +44,7 @@ var getJSONData = function(url){
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-// if(localStorage.length == 0){
+// if(localStorage.length === 0){
 //   window.location.href = "login.html";
 // }
 });
