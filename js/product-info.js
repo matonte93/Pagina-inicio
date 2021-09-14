30c9d34e6820423749b6ff9d23e590cc4558df57
Nicolas Caraballo <nicocaraballo1212@gmail.com>
2021-09-14 11:23:07 -0300

    14/9

--- js/product-info.js
+++ js/product-info.js
@@ -1,10 +1,10 @@
-var producto = {};
+var product = {};
 
-function MonstarProductoGaleria(array){
+function ShowProductGallery(array) {
 
     let htmlContentToAppend = "";
 
-    for(let i = 0; i < array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         let image = array[i];
 
         htmlContentToAppend += `
@@ -19,29 +19,71 @@ function MonstarProductoGaleria(array){
     }
 }
 
+function Showcomment(array) {
+
+    let htmlContentToAppend = "";
+
+    for (let i = 0; i < array.length; i++) {
+        let comment = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div>
+                <div class="col-3">
+                </div>
+                <div class="col">
+                    <div >
+                        <h4 class="mb-1">`+ comment.user + `</h4> <p>`+ comment.dateTime +`</p>
+                    </div>
+                    <div>
+                        <p>`+ comment.description + `</p>
+                    </div>
+                    <div>
+                    
+                    </div>
+                </div>
+                
+            </div>
+         </div>
+        `
+
+        document.getElementById("commentdiv").innerHTML = htmlContentToAppend;
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
-            producto = resultObj.data;
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            product = resultObj.data;
 
-            let productNameHTML  = document.getElementById("productName");
+            let productNameHTML = document.getElementById("productName");
             let productDescriptionHTML = document.getElementById("productDescription");
-            let productCostHTML = document.getElementById("Precio");
-            // let productCountHTML = document.getElementById("productCount");
+            let productCostHTML = document.getElementById("Cost");
+            let productSoldCountHTML = document.getElementById("soldcount");
             // let productCriteriaHTML = document.getElementById("productCriteria");
-        
-            productNameHTML.innerHTML = producto.name;
-            productDescriptionHTML.innerHTML = producto.description;
-            productCostHTML.innerHTML = producto.currency +" "+ producto.cost;
-            // productCountHTML.innerHTML = category.productCount;
-            // productCriteriaHTML.innerHTML = category.productCriteria;
+
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productCostHTML.innerHTML = product.currency + " " + product.cost;
+            productSoldCountHTML.innerHTML = "Vendidos: " + product.soldCount + " uds.";
+            
 
             //Muestro las imagenes en forma de galería
-            MonstarProductoGaleria(producto.images);
+            ShowProductGallery(product.images);
         }
     });
+
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj2) {
+        if (resultObj2.status === "ok") {
+            comments = resultObj2.data;
+
+            // let score = document.getElementById("score");
+
+            Showcomment(comments);
+        }
+    });
+
 });
