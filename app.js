0d699eab4bfcb1329afd2eb3b396e5b511051b7e
Nicolas Caraballo <nicocaraballo1212@gmail.com>
2021-11-19 14:17:53 -0300

    pautaIndividualEntrega8

--- app.js
+++ app.js
@@ -1,20 +1,72 @@
 const express = require('express')
+const cors = require("cors");
 const app = express()
-const port = 3000
+const PORT= 3000
 
 
 app.use(cors()); // Permite que el servidor pueda recibir peticiones desde diferentes dominios
 app.use(express.json()); // Permite que los parámetros de las peticiones sean recibidos como JSON
 app.use(express.urlencoded({ extended: false })); // Permite interpretar los datos recibidos en el cuerpo de las peticiones
 
-app.use(express.static('backend'));
+
+app.get("/check", (req, res) => {
+  res.send('Working....');
+  
+});
 
 
-app.get("/prueba", (req, res) => {
-  // res.send('Hola Mundoss')
-  res.json(prueba);
-})
+//Json Categorias
+app.get("/category", (req, res) => {
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  res.sendFile(__dirname + "/backend/category/all.json");
+});
+
+app.get("/category-info", (req, res) => {
+
+  res.sendFile(__dirname + "/backend/category/1234.json");
+});
+
+
+//Json Productos 
+app.get("/products", (req, res) => {
+
+  res.sendFile(__dirname + "/backend/product/all.json");
+});
+
+app.get("/product-info", (req, res) => {
+
+  res.sendFile(__dirname + "/backend/product/5678.json");
+});
+
+app.get("/product-comments", (req, res) => {
+
+  res.sendFile(__dirname + "/backend/product/5678-comments.json");
+});
+
+app.get("/product-publish", (req, res) => {
+
+  res.sendFile(__dirname + "/backend/product/publish.json");
+});
+
+
+//Json Carrito
+app.get("/cart-info", (req, res) => {
+
+  res.sendFile(__dirname + "/backend/cart/987.json");
+});
+
+app.get("/cart-buy", (req, res) => {
+
+  res.sendFile(__dirname + "/backend/cart/buy.json");
+});
+
+app.get("/cart-info2", (req, res) => {
+
+  res.sendFile(__dirname + "/backend/cart/654.json");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+});
+
--- frontEnd/js/init.js
+++ frontEnd/js/init.js
@@ -1,12 +1,31 @@
-const CATEGORIES_URL = "https://japdevdep.github.io/ecommerce-api/category/all.json";
-const PUBLISH_PRODUCT_URL = "https://japdevdep.github.io/ecommerce-api/product/publish.json";
-const CATEGORY_INFO_URL = "https://japdevdep.github.io/ecommerce-api/category/1234.json";
-const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json";
-const PRODUCT_INFO_URL = "https://japdevdep.github.io/ecommerce-api/product/5678.json";
-const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
-const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
-const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
-const CART_INFO_URL2 ="https://japdevdep.github.io/ecommerce-api/cart/654.json";
+
+const CATEGORIES_URL = "http://localhost:3000/category";
+// const CATEGORIES_URL = "https://japdevdep.github.io/ecommerce-api/category/all.json";
+
+const PUBLISH_PRODUCT_URL = "http://localhost:3000/product-publish";
+// const PUBLISH_PRODUCT_URL = "https://japdevdep.github.io/ecommerce-api/product/publish.json";
+
+const CATEGORY_INFO_URL = "http://localhost:3000/category-info";
+// const CATEGORY_INFO_URL = "https://japdevdep.github.io/ecommerce-api/category/1234.json";
+
+const PRODUCTS_URL = "http://localhost:3000/products";
+// const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json";
+
+
+const PRODUCT_INFO_URL = "http://localhost:3000/product-info";
+// const PRODUCT_INFO_URL = "https://japdevdep.github.io/ecommerce-api/product/5678.json";
+
+const PRODUCT_INFO_COMMENTS_URL = "http://localhost:3000/product-comments";
+// const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
+
+const CART_INFO_URL = "http://localhost:3000/cart-info";
+// const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
+
+const CART_BUY_URL = "http://localhost:3000/cart-buy";
+// const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
+
+const CART_INFO_URL2 ="http://localhost:3000/cart-info2";
+// const CART_INFO_URL2 ="https://japdevdep.github.io/ecommerce-api/cart/654.json";
 
 var showSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "block";
