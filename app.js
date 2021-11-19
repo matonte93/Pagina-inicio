5df504f4d9fa8a3fb5302970953c558b574c8ac9
Nicolas Caraballo <nicocaraballo1212@gmail.com>
2021-11-19 10:37:25 -0300

    11

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const prueba = ["hola","hola2, hola3"];
 
+app.use(cors()); // Permite que el servidor pueda recibir peticiones desde diferentes dominios
+app.use(express.json()); // Permite que los parámetros de las peticiones sean recibidos como JSON
+app.use(express.urlencoded({ extended: false })); // Permite interpretar los datos recibidos en el cuerpo de las peticiones
 
 app.use(express.static('backend'));
+
 
 app.get("/prueba", (req, res) => {
   // res.send('Hola Mundoss')
